82e2054497ceba06fb3b061bdb659905aebe1a20
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2023-08-03 15:46:19 +0200

    Sprint1V0: aggiunto text input per nome mappa, bug fix minori

--- projects/springIntro2023/src/main/java/unibo/springIntro2023/interfaces/MapService.java
+++
@@ -1,8 +0,0 @@
-package unibo.springIntro2023.interfaces;
-
-import unibo.springIntro2023.model.MapConfiguration;
-
-public interface MapService {
-
-    void dumpMap(MapConfiguration map);
-}
--- projects/springIntro2023/src/main/resources/static/js/utils.js
+++
@@ -1,8 +0,0 @@
-export function hasKey(map, value) {
-    for (const key of map.keys()) {
-        if (key === value) {
-            return true;
-        }
-    }
-    return false;
-}
