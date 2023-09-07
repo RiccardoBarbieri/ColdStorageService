8dadb247f2df4289f2c2089082daec79e9d0819e
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2023-09-07 18:52:53 +0200

    Sprint1V0: aggiornamento lib landmarks, serialVersionUID set

--- projects/unibo.landmarks/lib/src/main/java/unibo/landmarks/LandmarkCoordinates.java
+++ projects/unibo.landmarks/lib/src/main/java/unibo/landmarks/LandmarkCoordinates.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 public class LandmarkCoordinates implements Serializable {
 
+    private static final long serialVersionUID = 1L;
 
     private Map<String, List<Pair<Integer, Integer>>> coordinates;
 
