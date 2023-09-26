e244c870173ddc197972be45f9a70da9d617b8e0
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2023-09-26 23:54:50 +0200

    Sprint1V0: aggiunto trolleyexecutor

--- projects/sprint1V0/Arch_coldstorageservice23.py
+++ projects/sprint1V0/Arch_coldstorageservice23.py
@@ -24,12 +24,16 @@ with Diagram('coldstorageservice23Arch', show=False, outformat='png', graph_attr
      with Cluster('ctx_coldstorageservice', graph_attr=nodeattr):
           coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
           transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
+          trolleyexecutor=Custom('trolleyexecutor','./qakicons/symActorSmall.png')
      coldstorageservice >> Edge(color='blue', style='solid', xlabel='deposit', fontcolor='blue') >> transporttrolley
-     transporttrolley >> Edge(color='magenta', style='solid', xlabel='engage', fontcolor='magenta') >> basicrobot
-     transporttrolley >> Edge(color='blue', style='solid', xlabel='setrobotstate', fontcolor='blue') >> basicrobot
-     transporttrolley >> Edge( xlabel='alarm', **eventedgeattr, fontcolor='red') >> sys
-     transporttrolley >> Edge(color='magenta', style='solid', xlabel='getrobotstate', fontcolor='magenta') >> basicrobot
-     transporttrolley >> Edge(color='magenta', style='solid', xlabel='moverobot', fontcolor='magenta') >> basicrobot
+     transporttrolley >> Edge(color='magenta', style='solid', xlabel='getposition', fontcolor='magenta') >> trolleyexecutor
+     transporttrolley >> Edge(color='magenta', style='solid', xlabel='move', fontcolor='magenta') >> trolleyexecutor
      transporttrolley >> Edge(color='blue', style='solid', xlabel='chargetakentt', fontcolor='blue') >> coldstorageservice
      transporttrolley >> Edge(color='blue', style='solid', xlabel='chargedeposited', fontcolor='blue') >> coldstorageservice
+     trolleyexecutor >> Edge(color='magenta', style='solid', xlabel='engage', fontcolor='magenta') >> basicrobot
+     trolleyexecutor >> Edge(color='blue', style='solid', xlabel='fail', fontcolor='blue') >> transporttrolley
+     trolleyexecutor >> Edge(color='blue', style='solid', xlabel='setrobotstate', fontcolor='blue') >> basicrobot
+     trolleyexecutor >> Edge(color='magenta', style='solid', xlabel='moverobot', fontcolor='magenta') >> basicrobot
+     trolleyexecutor >> Edge( xlabel='alarm', **eventedgeattr, fontcolor='red') >> sys
+     trolleyexecutor >> Edge(color='magenta', style='solid', xlabel='getrobotstate', fontcolor='magenta') >> basicrobot
 diag
--- projects/sprint1V0/coldstorageservice23.pl
+++ projects/sprint1V0/coldstorageservice23.pl
@@ -5,4 +5,5 @@ context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
 context(ctx_coldstorageservice, "localhost",  "TCP", "8021").
  qactor( coldstorageservice, ctx_coldstorageservice, "it.unibo.coldstorageservice.Coldstorageservice").
   qactor( transporttrolley, ctx_coldstorageservice, "it.unibo.transporttrolley.Transporttrolley").
+  qactor( trolleyexecutor, ctx_coldstorageservice, "it.unibo.trolleyexecutor.Trolleyexecutor").
   qactor( basicrobot, ctxbasicrobot, "external").
