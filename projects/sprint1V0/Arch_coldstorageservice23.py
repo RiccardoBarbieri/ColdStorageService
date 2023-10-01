db66fa2af08b8f2ce557ec775e54d77d6e12ed97
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2023-10-01 17:03:58 +0200

    Correzione push errato

--- projects/sprint1V0/Arch_coldstorageservice23.py
+++ projects/sprint1V0/Arch_coldstorageservice23.py
@@ -1,3 +1,4 @@
+### conda install diagrams
 from diagrams import Cluster, Diagram, Edge
 from diagrams.custom import Custom
 import os
@@ -19,21 +20,27 @@ eventedgeattr = {
 with Diagram('coldstorageservice23Arch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
-     with Cluster('ctxbasicrobot', graph_attr=nodeattr):
-          basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
      with Cluster('ctx_coldstorageservice', graph_attr=nodeattr):
           coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
           transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
-          trolleyexecutor=Custom('trolleyexecutor','./qakicons/symActorSmall.png')
-     coldstorageservice >> Edge(color='blue', style='solid', xlabel='deposit', fontcolor='blue') >> transporttrolley
-     transporttrolley >> Edge(color='magenta', style='solid', xlabel='getposition', fontcolor='magenta') >> trolleyexecutor
-     transporttrolley >> Edge(color='magenta', style='solid', xlabel='move', fontcolor='magenta') >> trolleyexecutor
-     transporttrolley >> Edge(color='blue', style='solid', xlabel='chargetakentt', fontcolor='blue') >> coldstorageservice
-     transporttrolley >> Edge(color='blue', style='solid', xlabel='chargedeposited', fontcolor='blue') >> coldstorageservice
-     trolleyexecutor >> Edge(color='magenta', style='solid', xlabel='engage', fontcolor='magenta') >> basicrobot
-     trolleyexecutor >> Edge(color='blue', style='solid', xlabel='fail', fontcolor='blue') >> transporttrolley
-     trolleyexecutor >> Edge(color='blue', style='solid', xlabel='setrobotstate', fontcolor='blue') >> basicrobot
-     trolleyexecutor >> Edge(color='magenta', style='solid', xlabel='moverobot', fontcolor='magenta') >> basicrobot
-     trolleyexecutor >> Edge( xlabel='alarm', **eventedgeattr, fontcolor='red') >> sys
-     trolleyexecutor >> Edge(color='magenta', style='solid', xlabel='getrobotstate', fontcolor='magenta') >> basicrobot
+     with Cluster('ctx_access', graph_attr=nodeattr):
+          serviceaccessgui=Custom('serviceaccessgui','./qakicons/symActorSmall.png')
+     with Cluster('ctx_statusmonitor', graph_attr=nodeattr):
+          servicestatusgui=Custom('servicestatusgui','./qakicons/symActorSmall.png')
+     with Cluster('ctx_alarmsystem', graph_attr=nodeattr):
+          warningdevice=Custom('warningdevice','./qakicons/symActorSmall.png')
+          alarmdevice=Custom('alarmdevice','./qakicons/symActorSmall.png')
+     with Cluster('ctx_basicrobot', graph_attr=nodeattr):
+          basicrobotsim=Custom('basicrobotsim','./qakicons/symActorSmall.png')
+     coldstorageservice >> Edge( label='rejrequpdate', **eventedgeattr, fontcolor='red') >> sys
+     coldstorageservice >> Edge( label='coldroomupdate', **eventedgeattr, fontcolor='red') >> sys
+     transporttrolley >> Edge( label='statusupdate', **eventedgeattr, fontcolor='red') >> sys
+     serviceaccessgui >> Edge(color='magenta', style='solid', decorate='true', label='<storerequest &nbsp; codeentered &nbsp; >',  fontcolor='magenta') >> coldstorageservice
+     coldstorageservice >> Edge(color='blue', style='solid',  label='<newcharge &nbsp; >',  fontcolor='blue') >> transporttrolley
+     transporttrolley >> Edge(color='blue', style='solid',  label='<goToINDOOR &nbsp; goToColdRoom &nbsp; >',  fontcolor='blue') >> basicrobotsim
+     basicrobotsim >> Edge(color='blue', style='solid',  label='<arrivedINDOOR &nbsp; arrivedColdRoom &nbsp; arrivedHOME &nbsp; >',  fontcolor='blue') >> transporttrolley
+     transporttrolley >> Edge(color='blue', style='solid',  label='<chargedeposited &nbsp; >',  fontcolor='blue') >> coldstorageservice
+     alarmdevice >> Edge(color='blue', style='solid',  label='<stop &nbsp; resume &nbsp; >',  fontcolor='blue') >> transporttrolley
+     coldstorageservice >> Edge(color='blue', style='solid',  label='<chargetaken &nbsp; >',  fontcolor='blue') >> serviceaccessgui
 diag
--- projects/sprint1V0/coldstorageservice23.pl
+++ projects/sprint1V0/coldstorageservice23.pl
@@ -1,9 +1,33 @@
 %====================================================================================
 % coldstorageservice23 description   
 %====================================================================================
-context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
+request( storerequest, storerequest(FW) ).
+request( codeentered, codeentered(TICKET) ).
+event( rejrequpdate, rejrequpdate(NREJ) ).
+dispatch( newcharge, newcharge(FW) ).
+dispatch( chargetakentt, chargetakentt(arg) ).
+dispatch( chargetaken, chargetaken(TICKET) ).
+dispatch( chargedeposited, chargedeposited(FW) ).
+event( coldroomupdate, coldroomupdate(KG) ).
+event( statusupdate, statusupdate(POS,STATE) ).
+dispatch( stop, stop(arg) ).
+dispatch( resume, resume(arg) ).
+dispatch( goToINDOOR, goToINDOOR(arg) ).
+dispatch( goToColdRoom, goToColdRoom(arg) ).
+dispatch( goToHOME, goToHOME(arg) ).
+dispatch( arrivedINDOOR, arrivedINDOOR(arg) ).
+dispatch( arrivedColdRoom, arrivedColdRoom(arg) ).
+dispatch( arrivedHOME, arrivedHOME(arg) ).
+%====================================================================================
 context(ctx_coldstorageservice, "localhost",  "TCP", "8021").
+context(ctx_access, "localhost",  "TCP", "8022").
+context(ctx_statusmonitor, "localhost",  "TCP", "8023").
+context(ctx_alarmsystem, "localhost",  "TCP", "8024").
+context(ctx_basicrobot, "127.0.0.1",  "TCP", "8020").
  qactor( coldstorageservice, ctx_coldstorageservice, "it.unibo.coldstorageservice.Coldstorageservice").
+  qactor( basicrobotsim, ctx_basicrobot, "it.unibo.basicrobotsim.Basicrobotsim").
   qactor( transporttrolley, ctx_coldstorageservice, "it.unibo.transporttrolley.Transporttrolley").
-  qactor( trolleyexecutor, ctx_coldstorageservice, "it.unibo.trolleyexecutor.Trolleyexecutor").
-  qactor( basicrobot, ctxbasicrobot, "external").
+  qactor( serviceaccessgui, ctx_access, "it.unibo.serviceaccessgui.Serviceaccessgui").
+  qactor( servicestatusgui, ctx_statusmonitor, "it.unibo.servicestatusgui.Servicestatusgui").
+  qactor( warningdevice, ctx_alarmsystem, "it.unibo.warningdevice.Warningdevice").
+  qactor( alarmdevice, ctx_alarmsystem, "it.unibo.alarmdevice.Alarmdevice").
