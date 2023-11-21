5353fadc10628f62b3b7ee728d0bbf83133fe5af
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2023-11-21 11:53:56 +0100

    Sprint2V0: creato progetto sprint2

---
+++ projects/sprint2V0/coldstorageservice23.pl
@@ -0,0 +1,40 @@
+%====================================================================================
+% coldstorageservice23 description   
+%====================================================================================
+request( storerequest, storerequest(FW) ).
+request( chargestatus, chargestatus(arg) ).
+reply( loadaccepted, loadaccepted(arg) ).  %%for storerequest
+reply( loadrejected, loadrejected(arg) ).  %%for storerequest
+reply( chargetaken, chargetaken(arg) ).  %%for chargestatus
+reply( chargefailed, chargefailed(arg) ).  %%for chargestatus
+request( deposit, deposit(FW) ).
+request( depositstatus, depositstatus(arg) ).
+reply( chargetakentt, chargetakentt(FW) ).  %%for deposit
+reply( chargefailedtt, chargefailedtt(FW) ).  %%for deposit
+reply( chargedeposited, chargedeposited(FW) ).  %%for depositstatus
+reply( chargedepfailed, chargedeposited(FW) ).  %%for depositstatus
+request( move, move(X,Y) ).
+request( moveclosest, moveclosest(Xs,Ys) ).
+reply( movedone, movedone(arg) ).  %%for move
+reply( movefailed, movefailed(arg) ).  %%for move
+reply( movecdone, movecdone(arg) ).  %%for moveclosest
+reply( movecfailed, movecfailed(arg) ).  %%for moveclosest
+dispatch( fail, fail(ERROR) ).
+request( engage, engage(OWNER,STEPTIME) ).
+reply( engagedone, engagedone(ARG) ).  %%for engage
+reply( engagerefused, engagerefused(ARG) ).  %%for engage
+dispatch( disengage, disengage(ARG) ).
+request( moverobot, moverobot(TARGETX,TARGETY) ).
+reply( moverobotdone, moverobotok(ARG) ).  %%for moverobot
+reply( moverobotfailed, moverobotfailed(PLANDONE,PLANTODO) ).  %%for moverobot
+dispatch( setrobotstate, setpos(X,Y,D) ).
+event( alarm, alarm(X) ).
+request( getrobotstate, getrobotstate(ARG) ).
+reply( robotstate, robotstate(POS,DIR) ).  %%for getrobotstate
+%====================================================================================
+context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
+context(ctx_coldstorageservice, "localhost",  "TCP", "8021").
+ qactor( coldstorageservice, ctx_coldstorageservice, "it.unibo.coldstorageservice.Coldstorageservice").
+  qactor( transporttrolley, ctx_coldstorageservice, "it.unibo.transporttrolley.Transporttrolley").
+  qactor( trolleyexecutor, ctx_coldstorageservice, "it.unibo.trolleyexecutor.Trolleyexecutor").
+  qactor( basicrobot, ctxbasicrobot, "external").
